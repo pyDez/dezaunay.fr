cce23a19341b1aded3886dde6862db6fa504091f
Pierre-Yves Dezaunay <py.dezaunay@gmail.com>
2021-06-29 06:54:20 +0200

    init layout

---
+++ src/components/layouts/MainLayout.tsx
@@ -0,0 +1,12 @@
+import logo from '../../assets/logo-picto.svg'
+
+export default () => {
+    return (
+        <>
+            <a href='/'><img src={logo} alt='home'/></a>
+            <h2>Pierre-Yves</h2>
+            <h1>Dezaunay</h1>
+            <h3>Architecte & développeur indépendant</h3>
+        </>
+    );
+};
